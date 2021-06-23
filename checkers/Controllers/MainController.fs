652a28a196252adcb4d5669a04981aa969e0dff9
Jeyko Caicedo <jakeactually@outlook.com>
2021-06-23 10:44:37 -0500

    initial

---
+++ checkers/Controllers/MainController.fs
@@ -0,0 +1,119 @@
+﻿namespace checkers.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+open Microsoft.AspNetCore.Http
+open System.Text
+open Microsoft.FSharp.Control
+open System.Threading.Tasks
+open Room
+open System
+open System.Net.WebSockets
+open System.Threading
+
+[<ApiController>]
+[<Route("api/[action]")>]
+type MainController (logger : ILogger<MainController>, context : IHttpContextAccessor) =
+    inherit ControllerBase()
+
+    static let mutable rooms = Map.empty<int, Room>
+    static let mutable roomIndex = 0
+    static let mutable userIndex = 0
+
+    let helper = [None; Red; Blue; RedQueen; BlueQueen];
+    
+    let board =
+        Data.boardData |>
+        Array.map (fun row -> row |> Array.map (fun col -> helper.[col]))
+  
+    [<HttpGet>]
+    member __.Index() : int =
+        roomIndex <- roomIndex + 1
+        rooms <- rooms.Add (roomIndex, {
+            Id = roomIndex
+            Board = board
+            Users = List.empty
+            Turn = 0
+            Subscribers = List.empty
+         })
+        roomIndex
+
+    [<HttpGet("{id}")>]
+    member __.Room(id: int) : ObjectResult =
+        if not <| rooms.ContainsKey id then
+            upcast __.NotFound(())
+        else
+            let room = rooms.[id]
+            let userId = context.HttpContext.Session.GetInt32("userId").GetValueOrDefault(-1)
+
+            if room.Users |> List.contains userId then
+                let isFirst = room.Users.[0] = userId
+                upcast __.Ok(room.ForUser isFirst)
+            else
+                if room.Users.Length >= 2 then
+                    upcast __.Unauthorized("Room is already playing")
+                else if userId = -1 then                
+                    userIndex <- userIndex + 1
+                    room.Users <- List.append room.Users [userIndex]
+                    context.HttpContext.Session.SetInt32("userId", userIndex)
+                    let isFirst = room.Users.[0] = userIndex
+                    upcast __.Ok(room.ForUser isFirst)
+                else
+                    room.Users <- List.append room.Users [userId]
+                    let isFirst = room.Users.[0] = userId
+                    upcast __.Ok(room.ForUser isFirst)
+
+    [<HttpPost("{id}")>]
+    member __.Turn(id: int, path: Point[]) : Async<ObjectResult> = async {
+        let room = rooms.[id]
+        let userId = context.HttpContext.Session.GetInt32("userId").GetValueOrDefault(-1)
+        let isFirst = room.Users.[0] = userId
+        let userPath = Array.map (room.UserPoint isFirst) path
+        let From = userPath.[0]
+        let To = userPath.[userPath.Length - 1]
+        
+        if room.Turn % 2 >= room.Users.Length || room.Users.[room.Turn % 2] <> userId then
+            return upcast __.Unauthorized("Not your turn")         
+        else if helper.[room.Turn % 2 + 1].IsFoe room.Board.[From.Y].[From.X] then
+            return upcast __.BadRequest("You can't move enemy's pieces")
+        else if not <| room.validatePath isFirst userPath then
+            return upcast __.BadRequest("Invalid move")
+        else
+            room.Swap From To
+            room.Turn <- room.Turn + 1
+            room.Eat isFirst userPath
+
+            let goal = if isFirst then 0 else 7
+
+            if To.Y = goal then
+                let queen = if isFirst then RedQueen else BlueQueen
+                room.Board.[To.Y].[To.X] <- queen
+
+            for ws in room.Subscribers do                
+                let serverMsg = Encoding.UTF8.GetBytes("update")
+                let! _ = ws.SendAsync(new ArraySegment<byte>(serverMsg, 0, serverMsg.Length), WebSocketMessageType.Text, true, CancellationToken.None) |> Async.AwaitTask
+                ()               
+
+            return upcast __.Ok(room.ForUser isFirst)
+    }
+
+    [<HttpGet("{id}")>]
+    member __.State(id: int): Async<ObjectResult> = async {
+        let userId = context.HttpContext.Session.GetInt32("userId").GetValueOrDefault(-1)
+        
+        if not <| rooms.ContainsKey id then
+            return upcast __.NotFound(())
+        else
+            let room = rooms.[id]
+
+            if room.Users |> List.contains userId |> not then
+                return upcast __.Unauthorized(())         
+            else if context.HttpContext.WebSockets.IsWebSocketRequest then
+                let! webSocket = Async.AwaitTask <| context.HttpContext.WebSockets.AcceptWebSocketAsync()
+                room.Subscribers <- webSocket :: room.Subscribers
+                let tsc = new TaskCompletionSource<unit>()
+                let! _ = Async.AwaitTask tsc.Task
+                return upcast __.Ok(())
+            else
+                return upcast __.NotFound(())
+    }
---
+++ checkers/Models/Room.fs
@@ -0,0 +1,115 @@
+﻿module Room
+
+open System.Net.WebSockets
+open System
+
+[<Struct>]
+type Point = { mutable X: int; mutable Y: int }
+
+[<Struct>]
+type Move = { mutable From: Point; mutable To: Point }
+
+type Cell =
+    None | Red | Blue | RedQueen | BlueQueen
+    member __.IsRedPiece: bool = __ = Red || __ = RedQueen        
+    member __.IsFoe (other: Cell): bool = __.IsRedPiece <> other.IsRedPiece
+    member __.IsAlly (other: Cell): bool = __.IsRedPiece = other.IsRedPiece
+
+let getDiff (a: Point) (b: Point) : Point =
+    { X = b.X - a.X; Y = b.Y - a.Y }
+
+type Room =
+    {   Id: int
+        Board: Cell[][]
+        mutable Users: int list
+        mutable Turn: int
+        mutable Subscribers: WebSocket list }
+    
+    member __.Flip (): Room = {
+        __ with Board = [|
+            for y, row in Array.indexed __.Board do [|
+                for x, _ in Array.indexed row do
+                    __.Board.[7 - y].[7 - x]
+            |]
+        |]
+    }
+
+    member __.Swap (pointFrom: Point) (pointTo: Point): Unit =
+        let a = __.Board.[pointFrom.Y].[pointFrom.X]
+        let b = __.Board.[pointTo.Y].[pointTo.X]
+        __.Board.[pointFrom.Y].[pointFrom.X] <- b
+        __.Board.[pointTo.Y].[pointTo.X] <- a
+    
+    member __.Eat (isFirst: bool) (userPath: Point[]): Unit =
+        for point in userPath do
+            if isFirst && __.Board.[point.Y].[point.X] = Blue then
+                __.Board.[point.Y].[point.X] <- None
+
+            if not isFirst && __.Board.[point.Y].[point.X] = Red then
+                __.Board.[point.Y].[point.X] <- None
+
+    member __.ForUser (isFirst: bool): Room =
+        if isFirst then __ else __.Flip()
+
+    member __.UserPoint (isFirst: bool) (point: Point): Point =
+        if isFirst then point else { X = 7 - point.X; Y = 7 - point.Y }
+
+    member __.validatePath (isFirst: bool) (path: Point[]): bool =
+        let mutable valid = true
+        let mutable eated = false
+        let mutable rested = true
+        let mutable turned = false
+        let mutable lastDir: Option<Point> = Option.None
+
+        for i in 0..path.Length - 2 do
+            let From = path.[i]
+            let To = path.[i + 1]
+            let diff = getDiff From To
+            
+            System.Diagnostics.Trace.WriteLine(sprintf "From %O To %O Diff %O" From To diff)
+
+            if Math.Abs diff.Y > 1 then
+                valid <- false
+
+            if Option.exists (fun point -> point <> diff) lastDir then
+                if eated then
+                    turned <- true
+                    eated <- false
+                else
+                    valid <- false
+
+            let enemy = if isFirst then Blue else Red
+
+            if __.Board.[To.Y].[To.X] = enemy then
+                if rested then
+                    rested <- false
+                    eated <- true
+                else
+                    valid <- false
+
+            if __.Board.[To.Y].[To.X] = None then
+                rested <- true
+                            
+            System.Diagnostics.Trace.WriteLine(sprintf "i %O turned %O eated %O" i turned eated)
+
+            if i = path.Length - 2 then
+                if turned && not eated then
+                    valid <- false
+
+                if __.Board.[To.Y].[To.X] <> None then
+                    valid <- false
+                
+                let start = path.[0]
+                let player = __.Board.[start.Y].[start.X]
+
+                if player = Red || player = Blue then                
+                    System.Diagnostics.Trace.WriteLine(sprintf "len %O" path.Length)
+                    if path.Length = 3 && not eated then
+                        valid <- false
+
+                    if path.Length > 3 then
+                        valid <- false
+
+            lastDir <- Some(diff)
+        
+        valid
