0bff0608f7e84abb6f363b240f2c18efa0e8ca58
Jeyko Caicedo <jakeactually@outlook.com>
2024-10-24 16:45:17 -0500

    upgrade to net 8

--- checkers/Program.fs
+++ checkers/Program.fs
@@ -1,8 +1,17 @@
 namespace checkers
 #nowarn "20"
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
 
 module Program =
     let exitCode = 0
@@ -13,18 +22,16 @@ module Program =
         let builder = WebApplication.CreateBuilder(args)
 
         builder.Services.AddControllers()
-        builder.Services.AddMvc().AddSessionStateTempDataProvider()
         builder.Services.AddSession()
+        builder.Services.AddMvc().AddSessionStateTempDataProvider()
         builder.Services.AddHttpContextAccessor()
 
         let app = builder.Build()
 
-        app.UseHttpsRedirection()
-
         app.UseAuthorization()
+        app.MapControllers()
         app.UseSession()
         app.UseWebSockets()
-        app.MapControllers()
 
         app.Run()
 
